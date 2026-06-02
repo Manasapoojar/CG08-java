3568fd0eaa5c530b661d2a59fb3a061ebdbb9d3b
Manasa P R <manasa.pjr@gmail.com>
2026-06-02 13:35:40 +0530

    commit

--- corejavaprograms/src/com/tnsif/constructorprogram/Student.java
+++ corejavaprograms/src/com/tnsif/constructorprogram/Student.java
@@ -1,10 +1,14 @@
 package com.tnsif.constructorprogram;
 
 public class Student {
+	
+	 // Instance variables
 int id;
 String name;
 
-Student(int id,String name ){
+Student(int id,String name ){ // para const
+	
+	// 'this' refers to current object
 	this.id=id;
 	this.name=name;
 }
@@ -14,9 +18,15 @@ void display() {
 	System.out.println("Name :"+name);
 }
 public static void main(String[] args) {
+	
+	// Creating first Student object
 	Student s=new Student(101,"surya");
+	
+	// Creating sec Student object
 	Student s1=new Student(103,"yurya");
+	
 	// boolean output=s1.(instanceof)Student();
+	
 	s.display();
 	s1.display();
 }
